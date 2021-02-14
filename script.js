8fc951ca97eff96d20a1e940b480e951b80b0b40
Isac Homonik <dev.isachomonik@gmail.com>
2021-02-14 13:46:38 -0300

    Criado arquivo inicial.

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Escrito e Dirigido por:
+		Isac Homonik
+*/
+
+/*function msg(noem){
+	alert("Não clica aqui não vagabundo "+noem);
+}*/
+
+var A = 10;
+var B = 9;
+var X = A + B;
+
+console.log("X = "+X);
+
