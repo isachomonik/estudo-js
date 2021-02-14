b89556ccb99939342911ad68b357a8cf7c49bd3f
Isac Homonik <dev.isachomonik@gmail.com>
2021-02-14 15:40:50 -0300

    Função alteração de tamanho atráves de botão.

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ Escrito e Dirigido por:
 		Isac Homonik
 */
 
-/*function msg(noem){
-	alert("Não clica aqui não vagabundo "+noem);
-}*/
-
-var A = 10;
-var B = 19;
-var X = A + B;
-
-console.log("X = "+X);
-
+function changeSize(size){
+	var element = document.getElementById("mensagem");
+	element.style.fontSize = size; 
+}
